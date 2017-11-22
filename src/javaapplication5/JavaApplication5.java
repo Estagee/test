86d4989ac041d78bfd8648784358801025164988
patryk.redzinski <pedropatrykred@gmail.com>
2017-11-22 13:38:24 +0100

    trzeci

--- src/javaapplication5/JavaApplication5.java
+++ src/javaapplication5/JavaApplication5.java
@@ -16,6 +16,9 @@ public class JavaApplication5 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        String abcc = "dasads";
+
     }
     
 }
